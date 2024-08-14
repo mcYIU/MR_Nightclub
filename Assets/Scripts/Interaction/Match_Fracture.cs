3b9e61366ec821ac5839062f657b32fab5103644
mcYiu <mcyiu@tlu.ee>
2024-08-14 16:46:58 +0800

    Interaction Edit

--- Assets/Scripts/Interaction/Match_Fracture.cs
+++ Assets/Scripts/Interaction/Match_Fracture.cs
@@ -7,38 +7,20 @@ public class Match_Fracture : MonoBehaviour
     public HandGrabInteractor leftInteractor;
     public HandGrabInteractor rightInteractor;
 
+    HandGrabInteractable interactable;
+
+    private void Start()
+    {
+        interactable = GetComponent<HandGrabInteractable>();
+    }
+
     private void Update()
     {
-        if (leftInteractor.IsGrabbing == gameObject && rightInteractor.IsGrabbing == gameObject)
+        if (leftInteractor.SelectedInteractable == interactable && rightInteractor.SelectedInteractable == interactable)
         {
-            FractureObject();
-        }
-    }
-
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Environment"))
-        {
-            isGrabbed = false;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Environment"))
-        {
-            isGrabbed = true;
-        }
-    }*/
-
-    public void FractureObject()
-    {
-        //Debug.Log(interactable.Interactors.Count);
-        //if (interactable.Interactors.Count > 1)
-        //{
             Instantiate(fracturedObjectPrefab, transform.position, transform.rotation);
 
             Destroy(gameObject);
-        //}
+        }
     }
 }
