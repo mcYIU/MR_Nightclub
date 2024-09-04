605e3f5050cab0db8ffd82b0c2ff4202b75bd693
mcYiu <mcyiu@tlu.ee>
2024-09-04 16:38:43 +0800

    Update Balloon_Pop.cs

--- Assets/Scripts/Interaction/Balloon_Pop.cs
+++ Assets/Scripts/Interaction/Balloon_Pop.cs
@@ -4,24 +4,13 @@ public class Balloon_Pop : MonoBehaviour
 {
     public ParticleSystem explosionVFX;
     public InteractionManager interactionManager;
-    //int completedLevel = 0;
-    //int maxLevel = 2;
-    string objectName;
-
-    private void Start()
-    {
-        objectName = gameObject.name;
-    }
 
     public void Poke()
     {
-        //completedLevel++;
-        //Instantiate(explosionVFX, transform.position, Quaternion.identity);
-        //if (completedLevel == maxLevel)
-
         explosionVFX.Play();
         Destroy(gameObject);
 
-        interactionManager.ChangeLevelIndex(objectName);
+        if(interactionManager.LevelIndex < interactionManager.ineteractionLayerCount)
+            interactionManager.ChangeLevelIndex(gameObject.name);
     }
 }
