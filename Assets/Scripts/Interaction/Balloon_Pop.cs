fd0c0c669fa7ff1e19f6ec3d794470f3a33c73f0
mcYiu <mcyiu@tlu.ee>
2024-09-12 11:22:41 +0800

    Fix Interactions (added new assets)

--- Assets/Scripts/Interaction/Balloon_Pop.cs
+++ Assets/Scripts/Interaction/Balloon_Pop.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Balloon_Pop : MonoBehaviour
 {
-    public ParticleSystem explosionVFX;
     public InteractionManager interactionManager;
 
     public void Poke()
     {
-        explosionVFX.Play();
         Destroy(gameObject);
 
-        if(interactionManager.LevelIndex < interactionManager.ineteractionLayerCount)
+        if(interactionManager != null && interactionManager.LevelIndex < interactionManager.ineteractionLayerCount)
             interactionManager.ChangeLevelIndex(gameObject.name);
     }
 }
--- Assets/Scripts/Interaction/Rose.cs
+++ Assets/Scripts/Interaction/Rose.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class Rose : MonoBehaviour
 {
+    public MeshRenderer mesh;
     public InteractionManager interactionManager;
-    public int maxInteractionIndex = 2;
+    public int maxInteractionIndex;
 
     private int interactionIndex = 0;
-    
+
     public void AddIndex()
     {
         interactionIndex++;
         if (interactionIndex == maxInteractionIndex)
         {
+            if (mesh != null) mesh.enabled = false;
+
             interactionManager.ChangeLevelIndex(gameObject.name);
         }
     }
