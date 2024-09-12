1b585b6782ea459acb3968da9a77bdbb18e538b3
mcYiu <mcyiu@tlu.ee>
2024-09-12 15:48:34 +0800

    Clean Unused Asset

--- Assets/Scripts/GameLevelTrigger.cs
+++ Assets/Scripts/GameLevelTrigger.cs
@@ -2,54 +2,32 @@ using UnityEngine;
 
 public class GameLevelTrigger : MonoBehaviour
 {
-    public ParticleSystem[] customParticles;
-    public ParticleSystem mainParticle;
-    public Color initialColor;
-    public Color endColor;
+    public ParticleSystem startPoint;
+    public Animator NPC_Animator;
 
     private GameManager gameManager;
-    private CharacterTrailController trailController;
     private Collider triggerCollider;
 
     private void Start()
     {
         gameManager = FindAnyObjectByType<GameManager>();
-        trailController = FindAnyObjectByType<CharacterTrailController>();
-
         triggerCollider = GetComponent<Collider>();
-
-        EnableTriggerPoint();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (gameManager.isStarted)
             gameManager.ChangeToNextScene();
-
         else
-            trailController.ResetTrails();
+            Debug.Log("Play NPC Anim");
 
         DisableTriggerPoint();
         if(!gameManager.isStarted) gameManager.isStarted = true;
     }
 
-    public void EnableTriggerPoint()
-    {
-        for (int i = 0; i < customParticles.Length; i++)
-        {
-            ParticleSystem.MainModule particleMain = customParticles[i].main;
-            particleMain.startColor = new ParticleSystem.MinMaxGradient((!gameManager.isStarted) ? initialColor : endColor);
-        }
-
-        if (!triggerCollider.enabled) triggerCollider.enabled = true;
-        mainParticle.Play();
-    }
-
     private void DisableTriggerPoint()
     {
         triggerCollider.enabled = false;
-        mainParticle.Stop();
-
-        trailController.triggerPointTrail.Stop();
+        startPoint.Stop();
     }
 }
