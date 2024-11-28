3a4d5053041529f5a811a33758cc6f5ae4a06ff0
mcYiu <mcyiu@tlu.ee>
2024-11-28 18:25:16 +0800

    Update Dialogue.cs

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 [System.Serializable]
 public class Dialogue
 {
-   public string[] sentences;
+    public string[] sentences;
+    public AudioClip voiceOverAudio;
+    public AudioClip characterAudio;
+    [HideInInspector] public bool isVoiceOverEnded = false;
 }
