4feb7ad807161c8b4eb17a2fd1505700b0f43aef
mcYiu <mcyiu@tlu.ee>
2024-11-28 18:25:04 +0800

    Update EndSceneManager.cs

--- Assets/Scripts/EndSceneManager.cs
+++ Assets/Scripts/EndSceneManager.cs
@@ -7,7 +7,6 @@ public class EndSceneManager : MonoBehaviour
     public GameObject welcomeCharacter;
     public float audioDelay = 3.0f;
 
-    DialogueManager dialogueManager;
     OVRPassthroughLayer layer;
 
     void Start()
@@ -15,18 +14,14 @@ public class EndSceneManager : MonoBehaviour
         layer = FindObjectOfType<OVRPassthroughLayer>();
         if (layer.textureOpacity == 0f) layer.textureOpacity = 1.0f;
 
-        dialogueManager = FindObjectOfType<DialogueManager>();
         StartCoroutine(PlayWelcomeAudio());
     }
 
     private IEnumerator PlayWelcomeAudio()
     {
-        yield return new WaitForSeconds(audioDelay); 
+        yield return new WaitForSeconds(audioDelay);
 
-        if (dialogueManager.VO != null) dialogueManager.VO.PlayOneShot(welcomeAudio);
+        DialogueManager.OverrideInstructionAudio(welcomeAudio);
 
-        //yield return new WaitForSeconds(welcomeAudio.length);
-
-        //welcomeCharacter.SetActive(false);
     }
 }
