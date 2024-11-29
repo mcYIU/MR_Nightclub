d307a744f29ac3c77a4f3b175715e42b9f7bc364
mcYiu <mcyiu@tlu.ee>
2024-11-29 13:57:54 +0800

    Update Dice_Throw.cs

--- Assets/Scripts/Interaction/Dice_Throw.cs
+++ Assets/Scripts/Interaction/Dice_Throw.cs
@@ -1,58 +1,86 @@
-using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
 public class Dice_Throw : MonoBehaviour
 {
-    public InteractionManager interactionManager;
-    public AudioClip[] diceSound;
-    public float throwForce;
+    public static bool isGrabbed = false;
+    [SerializeField] private Interactable interactable;
+    [SerializeField] private AudioClip[] SFX;
+    [SerializeField] private float throwForce;
 
-    private bool isGrabbed = false;
-    private HandGrabInteractable grab;
-    private Rigidbody rb;
-    private AudioSource audioSource;
+    //private Rigidbody rb;
 
-    private void Start()
-    {
-        grab = GetComponent<HandGrabInteractable>();
-        rb = GetComponent<Rigidbody>();
-
-        audioSource = GetComponentInChildren<AudioSource>();
-    }
+    //private void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //}
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("Environment") && isGrabbed)
         {
-            int soundIndex = Random.Range(0, diceSound.Length);
-            audioSource.PlayOneShot(diceSound[soundIndex]);
-
-            interactionManager.ChangeLevelIndex(gameObject.name);
+            ThrowOnTable();
         }
     }
 
-    public void GrabAllDice(Transform parentDice)
+    private void ThrowOnTable()
     {
-        rb.useGravity = false;
-        rb.isKinematic = true;
-        rb.interpolation = RigidbodyInterpolation.None;
+        int soundIndex = Random.Range(0, SFX.Length);
+        SoundEffectManager.PlaySFXOnce(SFX[soundIndex]);
 
-        transform.position = parentDice.position;
-        transform.rotation = parentDice.rotation;
-        transform.SetParent(parentDice.transform);
-        transform.localScale = Vector3.one;
-
-        isGrabbed = true;
+        interactable.IncreaseInteractionLevel();
     }
 
-    public void ReleaseAllDice()
+    public void GrabAllDice(Transform[] otherTransforms, Rigidbody[] allRigidbodies)
     {
-        transform.SetParent(null);
-        transform.localScale = Vector3.one;
+        //rb.useGravity = false;
+        //rb.isKinematic = true;
+        //rb.interpolation = RigidbodyInterpolation.None;
 
-        rb.isKinematic = false;
-        rb.useGravity = true;
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
-        rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+        //transform.position = parentDice.position;
+        //transform.rotation = parentDice.rotation;
+        //transform.SetParent(parentDice.transform);
+        //transform.localScale = Vector3.one;
+
+        //isGrabbed = true;
+
+        foreach (Rigidbody rb in allRigidbodies)
+        {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            rb.interpolation = RigidbodyInterpolation.None;
+        }
+
+        foreach (Transform t in otherTransforms)
+        {          
+            t.position = transform.position;
+            t.rotation = transform.rotation;
+            t.SetParent(transform);
+            t.localScale = Vector3.one;
+        }
+    }
+
+    public void ReleaseAllDice(Transform[] otherTransforms, Rigidbody[] allRigidbodies)
+    {
+        //transform.SetParent(null);
+        //transform.localScale = Vector3.one;
+
+        //rb.isKinematic = false;
+        //rb.useGravity = true;
+        //rb.interpolation = RigidbodyInterpolation.Interpolate;
+        //rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+
+        foreach (Transform t in otherTransforms)
+        {
+            t.SetParent(null);
+            t.localScale = Vector3.one;
+        }
+
+        foreach (Rigidbody rb in allRigidbodies)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.interpolation = RigidbodyInterpolation.Interpolate;
+            rb.AddForce(rb.transform.forward * throwForce, ForceMode.Impulse);
+        }    
     }
 }
