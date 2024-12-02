278cc34e319b91857ae63979be1b356309cbd456
mcYiu <mcyiu@tlu.ee>
2024-12-02 18:46:19 +0800

    Update Dice_Throw.cs

--- Assets/Scripts/Interaction/Dice_Throw.cs
+++ Assets/Scripts/Interaction/Dice_Throw.cs
@@ -1,22 +1,23 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Dice_Throw : MonoBehaviour
 {
-    public static bool isGrabbed = false;
+    public static bool isGrounded = true;
     [SerializeField] private Interactable interactable;
     [SerializeField] private AudioClip[] SFX;
     [SerializeField] private float throwForce;
 
-    //private Rigidbody rb;
+    private Rigidbody rb;
 
-    //private void Start()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //}
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.gameObject.CompareTag("Environment") && isGrabbed)
+        if (collision.collider.gameObject.CompareTag("Environment") && !isGrounded)
         {
             ThrowOnTable();
         }
@@ -30,57 +31,35 @@ public class Dice_Throw : MonoBehaviour
         interactable.IncreaseInteractionLevel();
     }
 
-    public void GrabAllDice(Transform[] otherTransforms, Rigidbody[] allRigidbodies)
+    public void GrabAllDices(Transform parentDice)
     {
-        //rb.useGravity = false;
-        //rb.isKinematic = true;
-        //rb.interpolation = RigidbodyInterpolation.None;
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        rb.interpolation = RigidbodyInterpolation.None;
+        
+        transform.SetPositionAndRotation(parentDice.position, parentDice.rotation);
+        transform.SetParent(parentDice.transform);
+        transform.localScale = Vector3.one;
 
-        //transform.position = parentDice.position;
-        //transform.rotation = parentDice.rotation;
-        //transform.SetParent(parentDice.transform);
-        //transform.localScale = Vector3.one;
+        isGrounded = false;
 
-        //isGrabbed = true;
-
-        foreach (Rigidbody rb in allRigidbodies)
-        {
-            rb.useGravity = false;
-            rb.isKinematic = true;
-            rb.interpolation = RigidbodyInterpolation.None;
-        }
-
-        foreach (Transform t in otherTransforms)
-        {          
-            t.position = transform.position;
-            t.rotation = transform.rotation;
-            t.SetParent(transform);
-            t.localScale = Vector3.one;
-        }
+        SetUI(isGrounded);
     }
 
-    public void ReleaseAllDice(Transform[] otherTransforms, Rigidbody[] allRigidbodies)
+    public void ReleaseAllDices()
     {
-        //transform.SetParent(null);
-        //transform.localScale = Vector3.one;
+        transform.SetParent(null);
+        transform.localScale = Vector3.one;
 
-        //rb.isKinematic = false;
-        //rb.useGravity = true;
-        //rb.interpolation = RigidbodyInterpolation.Interpolate;
-        //rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
 
-        foreach (Transform t in otherTransforms)
-        {
-            t.SetParent(null);
-            t.localScale = Vector3.one;
-        }
+        rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+    }
 
-        foreach (Rigidbody rb in allRigidbodies)
-        {
-            rb.isKinematic = false;
-            rb.useGravity = true;
-            rb.interpolation = RigidbodyInterpolation.Interpolate;
-            rb.AddForce(rb.transform.forward * throwForce, ForceMode.Impulse);
-        }    
+    private void SetUI(bool isGrabbed)
+    {
+        interactable.SetUI(isGrabbed);
     }
 }
