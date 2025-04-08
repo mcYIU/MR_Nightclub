7e8d3f0cb7085cafe5ba6d0cb4edfb5efd69bfdf
mcYiu <mcyiu@tlu.ee>
2025-04-08 17:34:51 +0800

    Update Candle_Blow.cs

--- Assets/Scripts/Interaction/Candle_Blow.cs
+++ Assets/Scripts/Interaction/Candle_Blow.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class Candle_Blow : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer face;
-    [SerializeField] private int blendshapeIndex;
+    [SerializeField] private int[] blendshapeIndex;
     [SerializeField] private float triggerFloat;
     [SerializeField] private AudioClip SFX;
     [SerializeField] Interactable interactable;
 
     private void OnTriggerStay(Collider other)
     {
-        if (interactable.isInteractionEnabled)
+        if (interactable.isInteractionEnabled && other.GetComponent<SkinnedMeshRenderer>() == face)
         {
-            float weightIndex = face.GetBlendShapeWeight(blendshapeIndex);
-
-            if (weightIndex > triggerFloat)
+            if (CheckBlendshapeWeight())
             {
                 Blow();
             }
@@ -28,5 +26,23 @@ public class Candle_Blow : MonoBehaviour
         interactable.IncreaseInteractionLevel();
 
         Destroy(gameObject);
+    }
+
+    private float GetBlendshapeWeight(int index)
+    {
+        return face.GetBlendShapeWeight(index);
+    }
+
+    private bool CheckBlendshapeWeight()
+    {
+        int triggeredIndex = 0;
+
+        foreach (var _index in blendshapeIndex)
+        {
+            if (GetBlendshapeWeight(_index) > triggerFloat)
+                triggeredIndex++;
+        }
+
+        return triggeredIndex > 0;
     }
 }
