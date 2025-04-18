0e9b97366fb8842ada38627aa8377a3137c2e650
mcYiu <mcyiu@tlu.ee>
2025-04-18 13:35:50 +0800

    Update DanceTrigger.cs

--- Assets/Scripts/Movement/DanceTrigger.cs
+++ Assets/Scripts/Movement/DanceTrigger.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class DanceTrigger : MonoBehaviour
 {
+    [SerializeField] private SkinnedMeshRenderer target;
+    [SerializeField] private Color completionColor;
+    [SerializeField] DanceQuestManager manager;
+    private Material material;
+
     private void Start()
     {
-        
+        material = target.material;
     }
 
+    public void NextPose()
+    {
+        material.color = completionColor;
 
+        if (manager) manager.CheckPoseCompletion();
+    }
 }
